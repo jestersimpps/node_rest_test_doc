71f7336878d6e0959eb3dcce6be8516d0ffc7035
jestersimpps <jestersimpps@gmail.com>
2016-07-01 10:15:40 +0200

    update schema gen

--- helpers/schema_generator.js
+++ helpers/schema_generator.js
@@ -1,4 +1,4 @@
-// Plugin to pgenerate a schema
+// Plugin to generate api schema
 'use strict';
 
 // sg(schema, 'GET', `/${testData.routeName}`, `return an array of ${testData.routeName}`, testData, res.body);
@@ -18,93 +18,110 @@ module.exports = (request_type, route, description, postObject, responseBody) =>
 
 	//input params
 
-	if (request_type == 'POST' || request_type == 'PUT') {
+	if (request_type === 'POST' || request_type === 'PUT') {
 		let apiParams = {
-			title: paramTitle,
-			type: `${getType(postObject)}`,
-			properties: {}
-		}
-
-		for (var property in postObject) {
-			if (postObject.hasOwnProperty(property)) {
-				apiParams.properties[property] = {
-					description: `${getDescription(postObject[property])}`,
-					type: `${getType(postObject[property])}`
-				}
+				title: paramTitle,
+				type: `${getType(postObject)}`,
+				properties: getObject(postObject)
 			}
-		}
-
-		jsonfile.writeFile(`${schemaDir}${paramTitle}.json`, apiParams, function (err) {
-			err && console.error(err)
+			//read file to see if changes
+		jsonfile.readFile(`${schemaDir}${paramTitle}.json`, function (err, obj) {
+			//if file doesnt exist
+			if (err) {
+				jsonfile.writeFile(`${schemaDir}${paramTitle}.json`, apiParams, function (err) {
+					err && console.error(err)
+				})
+			}
 		})
+
 	}
 
 
 	//success params
 
-	let apiSuccess = {
-		title: successTitle,
-		type: `${getType(responseBody)}`
-	};
+	if (responseBody) {
+		let apiSuccess = {
+			title: successTitle,
+			type: `${getType(responseBody)}`
+		};
 
-	switch (getType(responseBody)) {
-	case 'array':
-		apiSuccess.items = {
-			title: description,
-			type: "object",
-			properties: {}
-		};
-		for (let property in responseBody[0]) {
-			if (responseBody[0].hasOwnProperty(property)) {
-				if (getType(responseBody[0][property]) == 'array') {
-					apiSuccess.items.properties[property] = {
-						title: `${getDescription(responseBody[0][property])}`,
-						items: { type: "object", description: property },
-						type: `array`,
-						description: 'array of '
-					}
-				} else {
-					apiSuccess.items.properties[property] = {
-						description: `${getDescription(responseBody[0][property])}`,
-						type: `${getType(responseBody[0][property])}`
-					}
-				}
+		switch (getType(responseBody)) {
+		case 'array':
+			apiSuccess.items = {
+				title: description,
+				type: "object",
+				properties: getArray(responseBody)
+			};
+			break;
+		case 'object':
+			apiSuccess.properties = getObject(responseBody);
+			break;
+		case 'number':
+			apiSuccess.description = "a number";
+			break;
+		case 'string':
+			apiSuccess.description = "a string";
+			break;
+		}
+		//read file to see if changes
+		jsonfile.readFile(`${schemaDir}${successTitle}.json`, function (err, obj) {
+			//if file doesnt exist
+			if (err) {
+				jsonfile.writeFile(`${schemaDir}${successTitle}.json`, apiSuccess, function (err) {
+					err && console.error(err)
+				})
 			}
-		};
-		break;
-	case 'object':
-		apiSuccess.properties = {};
-		for (let property in responseBody) {
-			if (responseBody.hasOwnProperty(property)) {
-				if (getType(responseBody[property]) == 'array') {
-					apiSuccess.properties[property] = {
-						title: `${getDescription(responseBody[property])}`,
-						items: { type: "object", description: property },
-						type: `array`,
-						description: 'array of '
-					}
-				} else {
-					apiSuccess.properties[property] = {
-						description: `${getDescription(responseBody[property])}`,
-						type: `${getType(responseBody[property])}`
-					}
-				}
-			}
-		};
-		break;
-	case 'number':
-		break;
-	case 'string':
-		break;
+		})
+
 	}
 
 
-	jsonfile.writeFile(`${schemaDir}${successTitle}.json`, apiSuccess, function (err) {
-		err && console.error(err)
-	})
-
 
 	//helper functions
+
+	function getArray(array) {
+		let schema = {};
+		for (let property in array[0]) {
+			if (array[0].hasOwnProperty(property)) {
+				if (getType(array[0][property]) === 'array') {
+					schema[property] = {
+						title: `${getDescription(array[0][property])}`,
+						items: { type: "object", description: getDescription(property) },
+						type: `array`,
+						description: 'array of '
+					}
+				} else {
+					schema[property] = {
+						description: `${getDescription(array[0][property])}`,
+						type: `${getType(array[0][property])}`
+					}
+				}
+			}
+		}
+		return schema;
+	}
+
+	function getObject(object) {
+		let schema = {};
+		for (let property in object) {
+			if (object.hasOwnProperty(property)) {
+				if (getType(object[property]) === 'array') {
+					schema[property] = {
+						title: `${getDescription(object[property])}`,
+						items: { type: "object", description: getDescription(property) },
+						type: `array`,
+						description: 'array of '
+					}
+				} else {
+					schema[property] = {
+						description: `${getDescription(object[property])}`,
+						type: `${getType(object[property])}`
+					}
+				}
+			}
+		}
+		return schema;
+	}
 
 	function slugify(text) {
 		return text.toString().toLowerCase()
@@ -120,8 +137,14 @@ module.exports = (request_type, route, description, postObject, responseBody) =>
 	}
 
 	function getDescription(property) {
-		return JSON.stringify(property);
+		if (getType(property) === 'object') {
+			return JSON.stringify(property, null, 2);
+		}
+		return property;
 	}
+
+
+
 
 
 };
